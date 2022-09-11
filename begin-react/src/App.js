cad9fe693d5bf180fce392bdd90e2edc8b34e3b0
nohyunha95 <nohyunha95@gmail.com>
2022-09-11 14:33:11 +0900

    여러개의 input상태관리, useRef, 배열 렌더링

--- begin-react/src/App.js
+++ begin-react/src/App.js
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useRef } from "react";
 import Hello from "./Hello";
 import "./App.css";
 import Wrapper from "./Wrapper";
 import Counter from "./Counter";
+import InputSample from "./InputSample";
+import UserList from "./UserList";
 
 function App() {
   const name = "react";
@@ -12,6 +14,29 @@ function App() {
     color: "aqua",
     fontSize: 24, // 기본 단위 px
     padding: "1rem", // 다른 단위 사용 시 문자열로 설정
+  };
+  const users = [
+    {
+      id: 1,
+      username: "velopert",
+      email: "public.velopert@gmail.com",
+    },
+    {
+      id: 2,
+      username: "tester",
+      email: "tester@example.com",
+    },
+    {
+      id: 3,
+      username: "liz",
+      email: "liz@example.com",
+    },
+  ];
+  const nextId = useRef(4);
+  const onCreate = () => {
+    // 나중에 구현할 배열에 항목 추가하는 로직
+    // ...
+    nextId.current += 1;
   };
   return (
     <>
@@ -23,6 +48,8 @@ function App() {
       <div style={style}>{name}</div>
       <div className="gray-box"></div>
       <Counter />
+      <InputSample></InputSample>
+      <UserList users={users}></UserList>
     </>
   );
 }
