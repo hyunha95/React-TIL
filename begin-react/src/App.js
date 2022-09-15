01c4c097ae1fca1d03f6737e8f4fb55efd981d84
nohyunha95 <nohyunha95@gmail.com>
2022-09-16 08:23:14 +0900

    todoList template

--- begin-react/src/App.js
+++ begin-react/src/App.js
@@ -1,10 +1,11 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import Hello from "./Hello";
 import "./App.css";
 import Wrapper from "./Wrapper";
 import Counter from "./Counter";
 import InputSample from "./InputSample";
 import UserList from "./UserList";
+import CreateUser from "./CreateUser";
 
 function App() {
   const name = "react";
@@ -15,7 +16,7 @@ function App() {
     fontSize: 24, // 기본 단위 px
     padding: "1rem", // 다른 단위 사용 시 문자열로 설정
   };
-  const users = [
+  const [users, setUsers] = useState([
     {
       id: 1,
       username: "velopert",
@@ -31,12 +32,34 @@ function App() {
       username: "liz",
       email: "liz@example.com",
     },
-  ];
+  ]);
   const nextId = useRef(4);
   const onCreate = () => {
-    // 나중에 구현할 배열에 항목 추가하는 로직
-    // ...
+    const user = {
+      id: nextId.current,
+      username,
+      email,
+    };
+    setUsers([...users, user]);
+
+    setInputs({
+      username: "",
+      email: "",
+    });
     nextId.current += 1;
+  };
+
+  const [inputs, setInputs] = useState({
+    username: "",
+    email: "",
+  });
+  const { username, email } = inputs;
+  const onChange = (e) => {
+    const { name, value } = e.target;
+    setInputs({
+      ...inputs,
+      [name]: value,
+    });
   };
   return (
     <>
@@ -49,7 +72,13 @@ function App() {
       <div className="gray-box"></div>
       <Counter />
       <InputSample></InputSample>
-      <UserList users={users}></UserList>
+      <CreateUser
+        username={username}
+        email={email}
+        onChange={onChange}
+        onCreate={onCreate}
+      />
+      <UserList users={users} />
     </>
   );
 }
