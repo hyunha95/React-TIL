b3ce1d5c36bce252d6b93c3d37b50e27f2db5810
nohyunha95 <nohyunha95@gmail.com>
2023-04-09 22:54:40 +0900

    react useReducer

--- react-complete-guide-useEffect-useReducer/src/components/Login/Login.js
+++ react-complete-guide-useEffect-useReducer/src/components/Login/Login.js
@@ -1,45 +1,67 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useReducer } from "react";
 
 import Card from "../UI/Card/Card";
 import classes from "./Login.module.css";
 import Button from "../UI/Button/Button";
 
+const emailReducer = (state, action) => {
+  if (action.type === "USER_INPUT") {
+    return { value: action.val, isValid: action.val.includes("@") };
+  }
+  if (action.type === "INPUT_BLUR") {
+    return { value: state.value, isValid: state.value.includes("@") };
+  }
+  return { value: "", isValid: false };
+};
+
 const Login = (props) => {
-  const [enteredEmail, setEnteredEmail] = useState("");
-  const [emailIsValid, setEmailIsValid] = useState();
+  // const [enteredEmail, setEnteredEmail] = useState("");
+  // const [emailIsValid, setEmailIsValid] = useState();
   const [enteredPassword, setEnteredPassword] = useState("");
   const [passwordIsValid, setPasswordIsValid] = useState();
   const [formIsValid, setFormIsValid] = useState(false);
+
+  const [emailState, dispatchEmail] = useReducer(emailReducer, {
+    value: "",
+    isValid: null,
+  });
 
   useEffect(() => {
     console.log("EFFECT RUNNING");
   }, []);
 
-  useEffect(() => {
-    const identifier = setTimeout(() => {
-      console.log("Checking form validity");
-      setFormIsValid(
-        enteredEmail.includes("@") && enteredPassword.trim().length > 6
-      );
-    }, 500);
+  // useEffect(() => {
+  //   const identifier = setTimeout(() => {
+  //     console.log("Checking form validity");
+  //     setFormIsValid(
+  //       enteredEmail.includes("@") && enteredPassword.trim().length > 6
+  //     );
+  //   }, 500);
 
-    // 클린업 함수: 클린업 함수는 처음 렌더링 될 때는 실행되지 않는다. 의존성이 바뀔때마다 실행된다.
-    return () => {
-      console.log("Clean up");
-      clearTimeout(identifier);
-    };
-  }, [enteredEmail, enteredPassword]); // state 변경 함수는 기본적으로 리액트에 의해 절대 변경되지 않도록 보장되기 때문에 useEffect 배열안에 넣지 않아도 된다.
+  //   // 클린업 함수: 클린업 함수는 처음 렌더링 될 때는 실행되지 않는다. 의존성이 바뀔때마다 실행된다.
+  //   return () => {
+  //     console.log("Clean up");
+  //     clearTimeout(identifier);
+  //   };
+  // }, [enteredEmail, enteredPassword]); // state 변경 함수는 기본적으로 리액트에 의해 절대 변경되지 않도록 보장되기 때문에 useEffect 배열안에 넣지 않아도 된다.
 
   const emailChangeHandler = (event) => {
-    setEnteredEmail(event.target.value);
+    dispatchEmail({ type: "USER_INPUT", val: event.target.value });
+
+    setFormIsValid(
+      event.target.value.includes("@") && enteredPassword.trim().length > 6
+    );
   };
 
   const passwordChangeHandler = (event) => {
     setEnteredPassword(event.target.value);
+
+    setFormIsValid(emailState.isValid && event.target.value.trim().length > 6);
   };
 
   const validateEmailHandler = () => {
-    setEmailIsValid(enteredEmail.includes("@"));
+    // setEmailIsValid(emailState.isValid);
+    dispatchEmail({ type: "INPUT_BLUR" });
   };
 
   const validatePasswordHandler = () => {
@@ -48,7 +70,7 @@ const Login = (props) => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    props.onLogin(enteredEmail, enteredPassword);
+    props.onLogin(emailState.value, enteredPassword);
   };
 
   return (
@@ -56,14 +78,14 @@ const Login = (props) => {
       <form onSubmit={submitHandler}>
         <div
           className={`${classes.control} ${
-            emailIsValid === false ? classes.invalid : ""
+            emailState.isValid === false ? classes.invalid : ""
           }`}
         >
           <label htmlFor="email">E-Mail</label>
           <input
             type="email"
             id="email"
-            value={enteredEmail}
+            value={emailState.value}
             onChange={emailChangeHandler}
             onBlur={validateEmailHandler}
           />
