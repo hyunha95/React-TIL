348dfe59072b41b627d555c310b7d552ed015b6d
nohyunha95 <nohyunha95@gmail.com>
2023-04-12 23:11:50 +0900

    장바구니에 추가하는 기능. useContext사용

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,29 +1,30 @@
-import { Fragment, useState } from "react";
+import { useState } from "react";
 
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 import Cart from "./components/Cart/Cart";
+import CartProvider from "./store/CartProvider";
 
 function App() {
-    const [cartIsShown, setCartIsShown] = useState(false);
+  const [cartIsShown, setCartIsShown] = useState(false);
 
-    const showCartHandler = () => {
-        setCartIsShown(true);
-    };
+  const showCartHandler = () => {
+    setCartIsShown(true);
+  };
 
-    const hideCartHandler = () => {
-        setCartIsShown(false);
-    };
+  const hideCartHandler = () => {
+    setCartIsShown(false);
+  };
 
-    return (
-        <Fragment>
-            {cartIsShown && <Cart onClose={hideCartHandler} />}
-            <Header onShowCart={showCartHandler} />
-            <main>
-                <Meals />
-            </main>
-        </Fragment>
-    );
+  return (
+    <CartProvider>
+      {cartIsShown && <Cart onClose={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
+      <main>
+        <Meals />
+      </main>
+    </CartProvider>
+  );
 }
 
 export default App;
