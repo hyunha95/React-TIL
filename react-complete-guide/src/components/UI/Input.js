8b6202de44355d5f564d166d828ab866082ce4b8
nohyunha95 <nohyunha95@gmail.com>
2023-04-14 23:38:14 +0900

    음식 주문 앱 완성

--- react-complete-guide/src/components/UI/Input.js
+++ react-complete-guide/src/components/UI/Input.js
@@ -3,6 +3,23 @@ import React from "react";
 import classes from "./Input.module.css";
 
 const Input = React.forwardRef((props, ref) => {
+  // global state
+
+  // state
+
+  // ref
+
+  // router-info
+
+  // read-only
+
+  // event
+
+  // method
+
+  // watch
+
+  // view
   return (
     <div className={classes.input}>
       <label htmlFor={props.input.id}>{props.label}</label>
